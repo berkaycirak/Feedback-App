1a28d3d2483cc7fb5cf16b0ec9ce873150e6f62d
Berkay <berkaycirak_11@hotmail.com>
2022-05-31 15:35:01 +0300

    Animation is added on the added and deleted items

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,16 +1,39 @@
+import { motion, AnimatePresence } from 'framer-motion';
 import FeedbackItem from './FeedbackItem';
 import PropTypes from 'prop-types';
 
 function FeedbackList({ feedback, handleDelete }) {
   return (
-    <div>
-      {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
-      ))}
+    <div className='feedback-list'>
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem
+              key={item.id}
+              item={item}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
 
-      {feedback.length === 0 && 'No Feedback Yet!'}
+        {feedback.length === 0 && 'No Feedback Yet!'}
+      </AnimatePresence>
     </div>
   );
+  // return (
+  //   <div className='feedback-list'>
+  //     {feedback.map((item) => (
+  //       <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+  //     ))}
+
+  //     {feedback.length === 0 && 'No Feedback Yet!'}
+  //   </div>
+  // );
 }
 
 FeedbackList.propTypes = {
