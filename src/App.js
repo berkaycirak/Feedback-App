2029bbc515b736d827a97e0852d3d9043f553b53
Berkay <berkaycirak_11@hotmail.com>
2022-05-31 17:41:10 +0300

    React Link is added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackList from './components/FeedbackList';
+import AboutIconLink from './components/AboutIconLink';
 import FeedbackData from './data/FeedbackData';
 import FeedbackForm from './components/FeedbackForm';
 import About from './pages/About';
@@ -46,6 +47,7 @@ function App() {
           <Route path='/about' element={<About />} />
         </Routes>
       </div>
+      <AboutIconLink />
     </Router>
   );
 }
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Card from '../components/shared/Card';
 
 function About() {
@@ -9,7 +10,7 @@ function About() {
         <p>Version: 1.0.0</p>
 
         <p>
-          <a href='/'>Back To Home, hello</a>
+          <Link to='/'>Back To Home</Link>
         </p>
       </div>
     </Card>
